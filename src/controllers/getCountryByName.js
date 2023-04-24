14f5bb6ca2f812cbfec60d68d32fc985295b0ba8
FacuCasado <facufcasado@gmail.com>
2023-04-24 15:56:21 -0300

    3

--- src/controllers/getCountryByName.js
+++ src/controllers/getCountryByName.js
@@ -1,4 +1,4 @@
-const {Country} = require('../db')
+const {Country, Activity} = require('../db')
 const { Op, sequelize } = require('sequelize');
 const getCountries = require('./getCountries');
 
@@ -10,7 +10,12 @@ const getCountryByName = async(req,res)=>{
             const founds = await Country.findAll({
                 where: {
                 name: {[Op.iLike]: `%${countryName}%`}
-                }
+                },
+                include: [{
+                    model: Activity,
+                    attributes: ['name', 'dificulty', 'duration', 'season'],
+                    through: { attributes: [] }
+                }]
             });
             if(founds.length>0){
                 res.status(200).json(founds)
